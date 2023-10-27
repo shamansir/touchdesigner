d8cb21523a3c545f29a8d9499502ec910deeebf7
Ulric Wilfred <shaman.sir@gmail.com>
2023-10-27 18:24:29 +0200

    halloween works, p.2

---
+++ circuit_tracks_sliders.py
@@ -0,0 +1,58 @@
+# synth 1 knobs 1-8
+s1	b0 50 --
+s2	b0 51 --
+s3	b0 52 --
+s4	b0 53 --
+s5	b0 54 --
+s6	b0 55 --
+s7	b0 56 --
+s8	b0 57 --
+# synth 2 knobs 1-8
+s11	b1 50 --
+s12	b1 51 --
+s13	b1 52 --
+s14	b1 53 --
+s15	b1 54 --
+s16	b1 55 --
+s17	b1 56 --
+s18	b1 57 --
+# midi 1 knobs 1-8
+s21	b2 01 --
+s22	b2 02 --
+s23	b2 05 --
+s24	b2 0b --
+s25	b2 0c --
+s26	b2 0d --
+s27	b2 47 --
+s28	b2 4a --
+# midi 2 knobs 1-8
+s31	b3 01 --
+s32	b3 02 --
+s33	b3 05 --
+s34	b3 0b --
+s35	b3 0c --
+s36	b3 0d --
+s37	b3 47 --
+s38	b3 4a --
+# drum 1 knobs 1-4
+s41	b9 0e --
+s42	b9 0f --
+s43	b9 10 --
+s44	b9 11 --
+# drum 2 knobs 1-4
+s51	b9 22 --
+s52	b9 28 --
+s53	b9 2a --
+s54	b9 2b --
+# drum 3 knobs 1-4
+s61	b9 2e --
+s62	b9 2f --
+s63	b9 30 --
+s64	b9 31 --
+# drum 4 knobs 1-4
+s71	b9 37 --
+s72	b9 39 --
+s73	b9 3d --
+s74	b9 4c --
+# filter knob
+s80 bf 4a --
